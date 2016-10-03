05ec82a487166f347d7f86d2155b3ce26b3dff73
tijsmallaerts <tijs.mallaerts@gmail.com>
2016-10-03 22:08:10 +0200

    Add requirement for emacs 24.4 due to advice-add

--- org-clock-today.el
+++ org-clock-today.el
@@ -4,6 +4,8 @@
 ;;
 ;; Author: Tijs Mallaerts <tijs.mallaerts@gmail.com>
 
+;; Package-Requires: ((emacs "24.4"))
+
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
