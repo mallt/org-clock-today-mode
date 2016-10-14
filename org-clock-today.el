02b8fd541a01040405a9a1400c46dcb68b7c2a3a
tijsmallaerts <tijs.mallaerts@gmail.com>
2016-10-14 11:20:12 +0200

    Update emacs dependency to version 25

--- org-clock-today.el
+++ org-clock-today.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Tijs Mallaerts <tijs.mallaerts@gmail.com>
 
-;; Package-Requires: ((emacs "24.4"))
+;; Package-Requires: ((emacs "25"))
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
